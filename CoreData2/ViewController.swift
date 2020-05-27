4389a991a9cfc2044c75f43c5ed0e5dc762be2c7
Oleg Savelev <olegsavelev@list.ru>
2020-05-27 22:37:22 +0500

    Initial Commit

---
+++ CoreData2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData2
+//
+//  Created by MackBook on 27.05.2020.
+//  Copyright © 2020 Oleg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
